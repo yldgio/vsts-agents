80f12e582874ca39fc5ad38120ecc7862c786bb3
giovanni ferrara <yldgio@gmail.com>
2021-02-28 19:31:36 +0100

    added mor vars & refactored syntax

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,63 +1,63 @@
-resource "azurerm_resource_group" "aci-rg" {
-  name     = "aci-vsts"
-  location = "westeurope"
+resource "azurerm_resource_group" "aci_rg" {
+  name     = "aci_vsts"
+  location = var.location
 }
 
 resource "random_id" "randomId" {
   keepers = {
     # Generate a new ID only when a new resource group is defined
-    resource_group = "${azurerm_resource_group.aci-rg.name}"
+    resource_group = azurerm_resource_group.aci_rg.name
   }
 
   byte_length = 8
 }
 
-resource "azurerm_storage_account" "aci-sa" {
+resource "azurerm_storage_account" "aci_sa" {
   name                = "acisa${random_id.randomId.hex}"
-  resource_group_name = "${azurerm_resource_group.aci-rg.name}"
-  location            = "${azurerm_resource_group.aci-rg.location}"
+  resource_group_name = azurerm_resource_group.aci_rg.name
+  location            = azurerm_resource_group.aci_rg.location
   account_tier        = "Standard"
 
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_share" "aci-share" {
-  name                 = "aci-vsts-share"
-  resource_group_name  = "${azurerm_resource_group.aci-rg.name}"
-  storage_account_name = "${azurerm_storage_account.aci-sa.name}"
+resource "azurerm_storage_share" "aci_share" {
+  name                 = "aci_vsts_share"
+  resource_group_name  = azurerm_resource_group.aci_rg.name
+  storage_account_name = azurerm_storage_account.aci_sa.name
 
   quota = 50
 }
 
-resource "azurerm_container_group" "aci-vsts" {
-  name                = "aci-agent"
-  location            = "${azurerm_resource_group.aci-rg.location}"
-  resource_group_name = "${azurerm_resource_group.aci-rg.name}"
+resource "azurerm_container_group" "aci_vsts" {
+  name                = var.container_group_name
+  location            = azurerm_resource_group.aci_rg.location
+  resource_group_name = azurerm_resource_group.aci_rg.name
   ip_address_type     = "public"
   os_type             = "linux"
 
   container {
-    name   = "vsts-agent-centos-7"
-    image  = "yldgio/vsts-agent:centos-7"
+    name   = var.container_name
+    image  = var.container_image
     cpu    = "0.5"
     memory = "1.5"
     port   = "80"
 
     environment_variables {
-      "VSTS_ACCOUNT" = "${var.vsts-account}"
-      "VSTS_TOKEN"   = "${var.vsts-token}"
-      "VSTS_AGENT"   = "${var.vsts-agent}"
-      "VSTS_POOL"    = "${var.vsts-pool}"
+      "VSTS_ACCOUNT" = var.vsts_account
+      "VSTS_TOKEN"   = var.vsts_token
+      "VSTS_AGENT"   = var.vsts_agent
+      "VSTS_POOL"    = var.vsts_pool
     }
 
     volume {
       name       = "logs"
       mount_path = "/aci/logs"
       read_only  = false
-      share_name = "${azurerm_storage_share.aci-share.name}"
+      share_name = azurerm_storage_share.aci_share.name
 
-      storage_account_name = "${azurerm_storage_account.aci-sa.name}"
-      storage_account_key  = "${azurerm_storage_account.aci-sa.primary_access_key}"
+      storage_account_name = azurerm_storage_account.aci_sa.name
+      storage_account_key  = azurerm_storage_account.aci_sa.primary_access_key
     }
   }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,34 @@
-variable "vsts-account" {
-  default = "eneros"
+variable "vsts_account" {
+  description = "Azuree Devops Organization Name"
 }
 
-variable "vsts-token" {
-  default = ""
+variable "vsts_token" {
+  description = "Azuree Devops Personal access Token"
 }
 
-variable "vsts-agent" {
+variable "vsts_agent" {
+  description = "Azuree Devops Agent Name"
   default = "ACI-Agent"
 }
 
-variable "vsts-pool" {
+variable "vsts_pool" {
+  description = "Azuree Devops Agent Pool Name"
   default = "ACI-Pool"
+}
+variable "container_group_name"{
+  description = "the name of the Azure container instance"
+  default = "vsts-agents"
+
+}
+variable "container_name" {
+  description = "the name of the container"
+  default = "vsts-agent"
+}
+variable "container_image" {
+  description = "docker image"
+  default = "yldgio/vsts-agent:centos-7"
+}
+variable "location" {
+  description = "region where the resources should exist"
+  default     = "westeurope"
 }
