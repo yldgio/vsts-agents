e64f86c810f4fa012fecdd65fe556f96c1a11be7
giovanni ferrara <yldgio@gmail.com>
2021-03-18 18:41:38 +0100

    adding new images to build

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+docker build -t vsts-container:centos-7-latest --build-arg VCS_REF="git rev-parse --short HEAD" ./centos-7-base/.
+docker tag vsts-container:centos-7-latest yldgio/vsts-container:centos-7-latest 
+docker push yldgio/vsts-container:centos-7-latest
+
+docker build -t vsts-container:centos-7-php --build-arg VCS_REF="git rev-parse --short HEAD" ./pipeline-containers/centos-7-php/.
+docker tag vsts-container:centos-7-php yldgio/vsts-container:centos-7-php 
+docker push yldgio/vsts-container:centos-7-php
+
 docker build -t vsts-agent:centos-7 --build-arg VCS_REF="git rev-parse --short HEAD" ./centos-7/.
 docker tag vsts-agent:centos-7 yldgio/vsts-agent:centos-7 
 docker push yldgio/vsts-agent:centos-7 
